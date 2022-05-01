0114eccb438e83828d5d97981ade8465badc623a
bhaiest <thevakhovske@petalmail.com>
2022-05-01 19:00:25 +0000

    fix rights and ship more stuff

--- profiledef.sh
+++ profiledef.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 iso_name="SteamOS_Holo"
-iso_label="JUPITER_$(date +%Y%m)"
+iso_label="HOLO_$(date +%Y%m)"
 iso_publisher="theVakhovske <https://github.com/bhaiest>"
 iso_application="SteamOS Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
@@ -20,4 +20,5 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/holoinstall"]="0:0:755"  
 )
