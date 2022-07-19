3aa546fb239fdf3bb6e431bc6e06475dbe4f8d46
Adam Jafarov <thevakhovske@petalmail.com>
2022-07-19 05:53:09 -0700

    ISO: Another touch-up

--- profiledef.sh
+++ profiledef.sh
@@ -22,5 +22,5 @@ file_permissions=(
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/usr/local/bin/holoinstall"]="0:0:755"
   ["/etc/skel/Desktop/ime-kb.desktop"]="0:0:755"  
-  ["/etc/skel/Desktop/Return.desktop"]="0:0:755"
+  ["/etc/xdg/autostart/folder-struct.desktop"]="0:0:755"
 )
