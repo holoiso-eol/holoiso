a533ce68a18d6cc84228a8f82b333552e3923663
Adam Jafarov <thevakhovske@petalmail.com>
2022-07-19 04:07:22 -0700

    ISO: Switch to KDE Plasma with Vapor

--- profiledef.sh
+++ profiledef.sh
@@ -4,7 +4,7 @@
 iso_name="HoloISO_Beta"
 iso_label="HOLO_$(date +%Y%m)"
 iso_publisher="theVakhovske <https://github.com/theVakhovskeIsTaken>"
-iso_application="SteamOS Installer media"
+iso_application="HoloISO Installer Image"
 iso_version="$(date +%Y%m%d_%H%M)"
 install_dir="arch"
 buildmodes=('iso')
@@ -20,5 +20,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/holoinstall"]="0:0:755"  
+  ["/usr/local/bin/holoinstall"]="0:0:755"
+  ["/etc/skel/Desktop/ime-kb.desktop"]="0:0:755"  
 )
