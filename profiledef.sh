d695c96d64aca8fa2bf508886b45804da62df3d2
Adam Jafarov <thevakhovske@petalmail.com>
2022-07-19 13:15:47 -0700

    ISO: Prepare for merge

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="HoloISO_Beta"
+iso_name="HoloISO_Stable_GUI"
 iso_label="HOLO_$(date +%Y%m)"
 iso_publisher="theVakhovske <https://github.com/theVakhovskeIsTaken>"
 iso_application="HoloISO Installer Image"
