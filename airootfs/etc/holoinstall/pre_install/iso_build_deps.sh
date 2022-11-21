f82f59b7ab828e674c512166ffd737ff574006e5
theVakhovskeIsTaken <thevakhovske@petalmail.com>
2022-11-21 23:09:37 +0300

    Lessen filesize by removing some shit

--- airootfs/etc/holoinstall/pre_install/iso_build_deps.sh
+++ airootfs/etc/holoinstall/pre_install/iso_build_deps.sh
@@ -4,6 +4,14 @@
 # Remove useless shortcuts for now
 mv /etc/skel/Desktop/Return.desktop /etc/holoinstall/post_install_shortcuts
 
+# Prepare thyself
+chmod +x /etc/holoinstall/post_install/install_holoiso.sh
+chmod +x /etc/skel/Desktop/install.desktop
+chmod 755 /etc/skel/Desktop/install.desktop
+
+# Remove stupid stuff on build
+rm /home/${LIVEOSUSER}/steam.desktop /home/${LIVEOSUSER}/steamos-gamemode.desktop
+
 # Add a liveOS user
 ROOTPASS="holoconfig"
 LIVEOSUSER="liveuser"
@@ -23,7 +31,7 @@ pacman -Sy
 
 # Install desktop suite
 pacman -Rcns --noconfirm pulseaudio xfce4-pulseaudio-plugin pulseaudio-alsa
-pacman -Rdd --noconfirm sddm
+pacman -Rdd --noconfirm sddm linux syslinux
 pacman --overwrite="*" --noconfirm -S holoiso-main holo/filesystem holoiso-updateclient wireplumber flatpak packagekit-qt5 rsync unzip sddm-wayland dkms steam-im-modules systemd-swap ttf-twemoji-default ttf-hack ttf-dejavu pkgconf pavucontrol partitionmanager gamemode lib32-gamemode cpupower bluez-plugins bluez-utils
 mv /etc/xdg/autostart/steam.desktop /etc/xdg/autostart/desktopshortcuts.desktop /etc/skel/Desktop/steamos-gamemode.desktop /etc/holoinstall/post_install_shortcuts
 wget https://gdrivecdn.thevakhovske.pw/6:/holoiso/os/x86_64/lib32-nvidia-utils-515.57-1-x86_64.pkg.tar.zst -P /etc/holoinstall/post_install/pkgs
@@ -44,14 +52,6 @@ rm /etc/mkinitcpio.conf
 mv /etc/mkinitcpio.conf.pacnew /etc/mkinitcpio.conf 
 rm /etc/mkinitcpio.d/* # This removes shitty unasked presets so that this thing can't overwrite it next time
 cp /etc/holoinstall/post_install/mkinitcpio_presets/linux-neptune.preset /etc/mkinitcpio.d/ # Yes. I'm lazy to use mkinitcpio-install. Problems? *gigachad posture*
-
-# Prepare thyself
-chmod +x /etc/holoinstall/post_install/install_holoiso.sh
-chmod +x /etc/skel/Desktop/install.desktop
-chmod 755 /etc/skel/Desktop/install.desktop
-
-# Remove stupid stuff on build
-rm /home/${LIVEOSUSER}/steam.desktop /home/${LIVEOSUSER}/steamos-gamemode.desktop
 
 # Remove this shit from post-build
 rm -rf /etc/holoinstall/pre_install
