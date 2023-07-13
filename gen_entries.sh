3b63d82de4407b305d87150b775d238537df4c78
theVakhovskeIsTaken <thevakhovske@petalmail.com>
2023-07-13 12:46:13 +0000

    Generate systemd-boot entry by checking bootstrap

---
+++ gen_entries.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PROFILEDIR=$1
+count=1
+for kernel in $(cat ${PROFILEDIR}/kernel_list.bootstrap)
+do
+    kernel=$(echo $kernel | sed 's/.*linux/linux/g' | sed 's/.*headers//g')
+    if [ -z "$kernel" ]; then
+        continue
+    fi
+    echo -e "title   HoloISO installer (${kernel}, Copy to RAM)\nlinux   /%INSTALL_DIR%/boot/x86_64/vmlinuz-${kernel}\ninitrd  /%INSTALL_DIR%/boot/intel-ucode.img\ninitrd  /%INSTALL_DIR%/boot/amd-ucode.img\ninitrd  /%INSTALL_DIR%/boot/x86_64/initramfs-${kernel}.img\noptions archisobasedir=%INSTALL_DIR% archisolabel=%ARCHISO_LABEL% cow_spacesize=10G copytoram" > ${PROFILEDIR}/efiboot/loader/entries/$count-$kernel-copytoram.conf
+    count=$((count+1))
+    echo -e "title   HoloISO installer (${kernel})\nlinux   /%INSTALL_DIR%/boot/x86_64/vmlinuz-${kernel}\ninitrd  /%INSTALL_DIR%/boot/intel-ucode.img\ninitrd  /%INSTALL_DIR%/boot/amd-ucode.img\ninitrd  /%INSTALL_DIR%/boot/x86_64/initramfs-${kernel}.img\noptions archisobasedir=%INSTALL_DIR% archisolabel=%ARCHISO_LABEL%" > ${PROFILEDIR}/efiboot/loader/entries/$count-$kernel.conf
+    count=$((count+1))
+done
